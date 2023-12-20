df3a489b065bd774f75b39686d42b47326d544b3
haseeblodhi46 <haseeblodhi46@gmail.com>
2023-12-21 04:07:02 +0500

    Navbar updated without any dynamic components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,22 @@
+
 import '../styles/Nav.css'
+import logo from '../assets/images/lemonlogovertical.svg';
+
 
 function Nav() {
     return (
-        <nav className = "Nav">
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+        <nav className="Nav">
+            <div className="Nav-logo">
+                <img src={logo} alt="Lemon Logo" />
+            </div>
+            <div className="Nav-options">
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </nav>
     );
 }
