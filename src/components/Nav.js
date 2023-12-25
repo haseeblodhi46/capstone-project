02769e51ec81f6b2737a27fb2769f3fc004558b6
haseeblodhi46 <haseeblodhi46@gmail.com>
2023-12-26 04:15:18 +0500

    Navbar made responsive with addition of hamburger menu icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 
 import '../styles/Nav.css'
 import logo from '../assets/images/lemonlogovertical.svg';
+import hamburger_icon from '../assets/images/icon_hamburger_menu.svg';
 
 
 function Nav() {
@@ -8,6 +9,9 @@ function Nav() {
         <nav className="Nav">
             <div className="Nav-logo">
                 <img src={logo} alt="Lemon Logo" />
+            </div>
+            <div className="Nav-hamburger-icon">
+                <img src={hamburger_icon} alt="Hamburger Icon" />
             </div>
             <div className="Nav-options">
                 <ul>
