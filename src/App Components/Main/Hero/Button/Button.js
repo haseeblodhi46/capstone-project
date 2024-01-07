489505dd000e89593cb88152478f5e77384bfdb1
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-07 10:33:12 +0500

    Button component added and Hero Section made responsive

---
+++ src/App Components/Main/Hero/Button/Button.js
@@ -0,0 +1,11 @@
+import './Button.css'
+
+function Button() {
+    return (
+        <button className = "Button">
+            Reserve a table
+        </button>
+    );
+}
+
+export default Button;
