4d4cb8ed100f63c3fb254ead55d914ac99a50a86
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-19 04:16:47 +0500

    Testimonials section made responsive

--- src/App Components/Main/Testimonials/Testimonials.js
+++ src/App Components/Main/Testimonials/Testimonials.js
@@ -1,10 +1,14 @@
 import './Testimonials.css';
+import Card2 from 'assets/reusable_components/Card2/Card2.js';
 
 const Testimonials = () => {
     return (
         <div className = "Testimonials">
             <h1>Testimonials</h1>
             <div className = "Testimonials-Container">
+                <Card2 idname = "first"/>
+                <Card2 idname = "second"/>
+                <Card2 idname = "third"/>
             </div>
         </div>
     );
---
+++ src/assets/reusable_components/Card2/Card2.js
@@ -0,0 +1,10 @@
+import './Card2.css'
+
+function Card2(props) {
+    return (
+        <div className = "Card2" id = {props.idname} >
+        </div>
+    );
+}
+
+export default Card2;
