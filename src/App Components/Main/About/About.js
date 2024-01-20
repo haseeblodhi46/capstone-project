7b4172c996f35b532cf7d1c90f4948c6cdf6e91d
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-20 16:41:41 +0500

    Started working on about section

--- src/App Components/Main/About/About.js
+++ src/App Components/Main/About/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './About.css'; // Updated import statement
+import resturant_chef from 'assets/images/restaurantchefB.jpg';
 const About = () => {
     return (
         <div className = "About">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
+            <img src={resturant_chef} height={270} width='100%'></img>
         </div>
     );
 };
