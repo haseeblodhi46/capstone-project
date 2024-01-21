d523707437559fc516cb0dd9dcc0f1e9ebafbcb9
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-22 03:07:20 +0500

    Make about sectin responsive

--- src/App Components/Main/About/About.js
+++ src/App Components/Main/About/About.js
@@ -6,8 +6,10 @@ const About = () => {
         <div className = "About">
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
-            <img src={resturant_chef} height={270} width='100%'></img>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist and fellation wna what not for content, keeping adding lemons</p>
+            <div className = "image-div">
+                <img src={resturant_chef} height={270} width='100%'></img>
+            </div>
         </div>
     );
 };
