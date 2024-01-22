c5b63b663dd2596d4e1c11a19add6a56a1662202
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-23 02:28:51 +0500

    Developing About Section: Learned how to place image relative to normal flow

--- src/App Components/Main/About/About.js
+++ src/App Components/Main/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css'; // Updated import statement
-import resturant_chef from 'assets/images/restaurantchefB.jpg';
+import resturant_chefA from 'assets/images/restaurantchefA.jpg';
+import resturant_chefB from 'assets/images/restaurantchefB.jpg';
 const About = () => {
     return (
         <div className = "About">
@@ -8,7 +9,8 @@ const About = () => {
             <h2>Chicago</h2>
             <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist and fellation wna what not for content, keeping adding lemons</p>
             <div className = "image-div">
-                <img src={resturant_chef} height={270} width='100%'></img>
+                <img src={resturant_chefB} height={270} width='100%'></img>
+                <img src={resturant_chefA} height={270} width='100%'></img>
             </div>
         </div>
     );
