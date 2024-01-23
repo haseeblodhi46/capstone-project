5d3684330719d4b1176550a6c98d7840c7096671
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-01-24 02:39:25 +0500

    Developed About section and made it responsive

--- src/App Components/Main/About/About.js
+++ src/App Components/Main/About/About.js
@@ -7,7 +7,8 @@ const About = () => {
         <div className = "About">
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist and fellation wna what not for content, keeping adding lemons</p>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist and fellation wna what not for content, keeping adding lemons. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat </p>
             <div className = "image-div">
                 <img src={resturant_chefB} height={270} width='100%'></img>
                 <img src={resturant_chefA} height={270} width='100%'></img>
