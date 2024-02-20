396c0b9fb7ed39b5f4b9086054e9781ddd280826
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-02-20 06:58:39 +0500

    Adjust offset of hero accordin got nav height Adjust offset offset of main according to navheight

--- src/App Components/Main/Hero/Hero.js
+++ src/App Components/Main/Hero/Hero.js
@@ -1,11 +1,26 @@
 import './Hero.css'
 import resturant_food from 'assets/images/restaurantfood.jpg';
 import Button from 'assets/reusable_components/Button/Button.js';
-
+import React, { useEffect, useState, useRef} from "react";
 
 function Hero() {
+
+   const [navHeight, setNavHeight] = useState(0);
+
+    useEffect(() => {
+        let nav = document.querySelector('.Nav');
+        setNavHeight(nav.clientHeight);
+        console.log(navHeight);
+    }, [navHeight]);
+
+    const hero_style = {
+        position: 'relative',
+        top: navHeight+'px',
+    }
+
+
     return (
-        <div className = "Hero">
+        <div className = "Hero" style = {hero_style}>
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
             <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
