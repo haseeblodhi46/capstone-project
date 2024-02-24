da4e4133d593d1cfb34fc87c16c19fe5b64cfe31
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-02-24 19:40:29 +0500

    Working on booking page

--- src/App Components/BookingPage/BookingComponent/BookingComponent.js
+++ src/App Components/BookingPage/BookingComponent/BookingComponent.js
@@ -1,5 +1,9 @@
-import './BookingComponent.css'
+import './BookingComponent.css';
 import React, { useEffect, useState, useRef} from "react";
+import { DatePicker_FlowBite } from 'flowbite-react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import './custom-datepicker.css';
 
 
 function BookingComponent() {
@@ -27,23 +31,29 @@ function BookingComponent() {
         console.log('Time: ' + timeValue);
         console.log('Guests: ' + guestsValue);
         console.log('Occasion: ' + occasionValue);
+        setDateValue('');
+        setTimeValue('12:00');
+        setGuestsValue('1');
+        setOccasionValue('Birthday');
     };
     return (
         <div className = "BookingComponent">
             <form style={form_style} onSubmit={handleSubmit}>
                 <label htmlFor="res-date">Choose Date:</label>
-                <input type="date" id="res-date" name="res-date" value={dateValue} onChange={e => setDateValue(e.target.value)}></input>
+                <input  type="date" className="dateClass" id="res-date" name="res-date" value={dateValue} onChange={e => setDateValue(e.target.value)}></input>
+                <DatePicker selected={dateValue} onChange={date => setDateValue(date)} className='custom-datepicker'/>
                 <label htmlFor="res-time">Choose Time:</label>
                 <select id="res-time" name="res-time" value={timeValue} onChange={e => setTimeValue(e.target.value)}>
                     {time_values.map(makeOption)}
                 </select>
                 <label htmlFor="res-guests">Number of guests:</label>
-                <input type="number" id="res-guests" name="res-guests" placeholder='1' min="1" max="10"></input>
+                <input type="number" id="res-guests" name="res-guests" placeholder='1' min="1" max="10" value={guestsValue} onChange={e => setGuestsValue(e.target.value)}></input>
                 <label htmlFor="res-occasion">Occasion:</label>
                 <select id="res-occasion" name="res-occasion" value={occasionValue} onChange = {e => setOccasionValue(e.target.value)}>
                     {occasion_values.map(makeOption)}
                 </select>
                 <input type="submit" value="Make Your Reservation"></input>
+                <DatePicker/>
             </form>
         </div>
     )
