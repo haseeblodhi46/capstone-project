96f68b3ddd25015f24ed067ae1d8774cc633b341
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-02-26 07:04:47 +0500

    styling form components, later it responsive will be added

--- src/App Components/BookingPage/BookingComponent/BookingComponent.js
+++ src/App Components/BookingPage/BookingComponent/BookingComponent.js
@@ -10,8 +10,10 @@ function BookingComponent() {
 
     const form_style = {
         display: 'grid',
-        maxWidth: '200px',
-        gap: 'auto'
+        width: '500px',
+        gap: 'auto',
+        height: '90%',
+        alignItems: 'center',
     };
 
     var time_values = ['12:00', '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30'];
@@ -20,10 +22,11 @@ function BookingComponent() {
         return(<option value = {X}>{X}</option>)
     }
 
-    const [dateValue, setDateValue] = useState('');
+    const [dateValue, setDateValue] = useState(null);
     const [timeValue, setTimeValue] = useState('12:00');
     const [guestsValue, setGuestsValue] = useState('1');
     const [occasionValue, setOccasionValue] = useState('Birthday');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -36,12 +39,19 @@ function BookingComponent() {
         setGuestsValue('1');
         setOccasionValue('Birthday');
     };
+
     return (
         <div className = "BookingComponent">
             <form style={form_style} onSubmit={handleSubmit}>
                 <label htmlFor="res-date">Choose Date:</label>
-                <input  type="date" className="dateClass" id="res-date" name="res-date" value={dateValue} onChange={e => setDateValue(e.target.value)}></input>
-                <DatePicker selected={dateValue} onChange={date => setDateValue(date)} className='custom-datepicker'/>
+                {/*<input  type="date" className="dateClass" id="res-date" name="res-date" value={dateValue} onChange={e => setDateValue(e.target.value)}></input>*/}
+                <DatePicker
+                selected={dateValue}
+                onChange={date => setDateValue(date)}
+                id='res-date'
+                popperPlacement='bottom'
+                placeholderText='Select Date'
+                className='datepickerinput'/>
                 <label htmlFor="res-time">Choose Time:</label>
                 <select id="res-time" name="res-time" value={timeValue} onChange={e => setTimeValue(e.target.value)}>
                     {time_values.map(makeOption)}
@@ -53,7 +63,6 @@ function BookingComponent() {
                     {occasion_values.map(makeOption)}
                 </select>
                 <input type="submit" value="Make Your Reservation"></input>
-                <DatePicker/>
             </form>
         </div>
     )
