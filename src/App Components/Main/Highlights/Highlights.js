5748e562f6c2950ab71a2a8d5881f088edaae931
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-03-28 05:30:00 +0500

    Styling custom cards

--- src/App Components/Main/Highlights/Highlights.js
+++ src/App Components/Main/Highlights/Highlights.js
@@ -1,14 +1,17 @@
 import './Highlights.css'
 import Button from 'assets/reusable_components/Button/Button.js';
 import Card from 'assets/reusable_components/Card/Card.js';
+import bruchetta from 'assets/images/bruchetta.png';
+import greek_salad from 'assets/images/greek salad.jpg';
+import lemon_dessert from 'assets/images/lemon dessert.jpg';
 
 function Highlights() {
     return (
         <div className = "Highlights">
             <h1>This Weeks Specials!</h1>
-            <Card idname = "first"/>
-            <Card idname = "second"/>
-            <Card idname = "third"/>
+            <Card idname = "first" image = {bruchetta} foodname = {"Bruchetta"}/>
+            <Card idname = "second" image = {greek_salad} foodname = {"Greek Salad"}/>
+            <Card idname = "third"  image = {lemon_dessert} foodname = {"Lemon Dessert"}/>
             <Button text={'Online Menu'}/>
         </div>
     );
--- src/assets/reusable_components/Card/Card.js
+++ src/assets/reusable_components/Card/Card.js
@@ -3,6 +3,8 @@ import './Card.css'
 function Card(props) {
     return (
         <div className = "Card" id = {props.idname} >
+            <img src={props.image} ></img>
+            <h1>{props.foodname}</h1>
         </div>
     );
 }
