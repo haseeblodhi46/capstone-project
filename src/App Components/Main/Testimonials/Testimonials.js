8b8f2699774751210e0c850a098b37ef6dc8fbe6
haseeblodhi46 <haseeblodhi46@gmail.com>
2024-03-30 16:30:38 +0500

    Still working on testimonials

--- src/App Components/Main/Testimonials/Testimonials.js
+++ src/App Components/Main/Testimonials/Testimonials.js
@@ -1,14 +1,17 @@
 import './Testimonials.css';
 import Card2 from 'assets/reusable_components/Card2/Card2.js';
+import claire from 'assets/images/claire.jpg';
+import jake from 'assets/images/jake.png';
+import sherry from 'assets/images/sherry.jpg';
 
 const Testimonials = () => {
     return (
         <div className = "Testimonials">
             <h1>Testimonials</h1>
             <div className = "Testimonials-Container">
-                <Card2 idname = "first"/>
-                <Card2 idname = "second"/>
-                <Card2 idname = "third"/>
+                <Card2 idname = "first" image = {claire} name = {'Claire'}/>
+                <Card2 idname = "second" image = {jake} name = {'Jake'}/>
+                <Card2 idname = "third" image = {sherry} name = {'Sherry'}/>
             </div>
         </div>
     );
--- src/assets/reusable_components/Card2/Card2.js
+++ src/assets/reusable_components/Card2/Card2.js
@@ -3,6 +3,10 @@ import './Card2.css'
 function Card2(props) {
     return (
         <div className = "Card2" id = {props.idname} >
+            <div className = "image-description-container">
+                <img src={props.image} alt={props.alt}></img>
+                <p>{props.name}</p>
+            </div>
         </div>
     );
 }
